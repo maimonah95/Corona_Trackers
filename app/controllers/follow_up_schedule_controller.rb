35a4d0e99b7a324d4e896b1f2a728820c510ab30
maimonah95 <malhwaymil@gmail.com>
2020-04-05 12:03:21 +0300

    Done view Schedule By the Day

--- app/controllers/follow_up_schedule_controller.rb
+++ app/controllers/follow_up_schedule_controller.rb
@@ -1,5 +1,11 @@
 class FollowUpScheduleController < ApplicationController
+    def show
+  @follow = Follow.find(params[:id])
+end
     def index
        @follow = Follow.all
-	end
+    end
+    def new
+      @follow = Follow.new
+    end
 end
